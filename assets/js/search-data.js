8293b0ee1b75f8729e55677d53c376d1bba616bb
ivezicV <52571839+ivezicV@users.noreply.github.com>
2025-09-10 19:34:40 +0000

    Deploying to gh-pages from @ ivezicV/ivezicV.github.io@e2a7ba2464d532a915af926fd771e0c619606298 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-a-simple-inline-announcement-hi",
+          title: 'A simple inline announcement. hi',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
