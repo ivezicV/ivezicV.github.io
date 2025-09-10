261d523853d5b039db27cc634774e9bdcada24ac
ivezicV <52571839+ivezicV@users.noreply.github.com>
2025-09-10 23:12:42 +0000

    Deploying to gh-pages from @ ivezicV/ivezicV.github.io@ba40eca25cbfd200589a225cbcbb0a7df1124198 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -419,16 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-presented-cytofm-the-first-cytology-foundation-model-at-the-10th-workshop-on-computer-vision-for-microscopy-image-analysis-at-cvpr-2025",
+          title: 'Presented CytoFM: The first cytology foundation model at the 10th Workshop on Computer...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -487,7 +465,7 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/iveicV", "_blank");
+          window.open("https://github.com/ivezicV", "_blank");
         },
       },{
         id: 'social-orcid',
